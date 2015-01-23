6276412ed91497260a093f23860115665bf5f49d
Adriano Pauli <adrianopauli03@gmail.com>
2015-01-22 22:44:20 -0200

    Cometario inserido em TesteFatoria.java

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -1,5 +1,5 @@
 public class TesteFatorial {
-
+	//Cometario inserido em TesteFatorial.java
 	public static void main(String [] agrs){
 		Fatorial fat = new Fatorial();
 		int resultado = fat.calculaFatorial(0);
